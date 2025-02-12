418e15891bfbf0060aac8632f0ff59a511b2b705
Milon Chandra Das <milonofficial53@gmail.com>
2025-02-12 16:12:28 +0600

    refactor: improve newsfeed layout for better responsiveness and update component naming

--- src/app/newsfeed/page.tsx
+++ src/app/newsfeed/page.tsx
@@ -1,21 +1,26 @@
-import Leftside from "@/components/main/leftside"
-import Rightside from "@/components/main/rightside"
-import Feed from "@/components/main/feed"
+import Leftside from "@/components/main/leftside";
+import Rightside from "@/components/main/rightside";
+import Feed from "@/components/main/feed";
 
-const pages = () => {
+const Pages = () => {
   return (
-    <div className="flex w-full">
-      <div className="w-1/4">
+    <div className="flex flex-col lg:flex-row min-h-screen relative">
+      {/* Left Sidebar */}
+      <div className="hidden lg:block lg:w-1/4 xl:w-1/5 fixed left-0">
         <Leftside />
       </div>
-      <div className="w-full">
+
+      {/* Main Feed */}
+      <div className="w-full lg:w-1/2 xl:w-3/5 lg:ml-[25%] xl:ml-[20%] lg:mr-[25%] xl:mr-[20%]">
         <Feed />
       </div>
-      <div className="w-1/4">
+
+      {/* Right Sidebar */}
+      <div className="hidden lg:block lg:w-1/4 xl:w-1/5 fixed right-0">
         <Rightside />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default pages
+export default Pages;
