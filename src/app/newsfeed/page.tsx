5d9e2a266399ce30509f0a4526b0b3e78c7b6f22
shhossain <hossain0338@gmail.com>
2025-02-12 16:46:30 +0600

    refactor: rename Pages component to pages and simplify layout structure

--- src/app/newsfeed/page.tsx
+++ src/app/newsfeed/page.tsx
@@ -2,25 +2,20 @@ import Leftside from "@/components/main/leftside";
 import Rightside from "@/components/main/rightside";
 import Feed from "@/components/main/feed";
 
-const Pages = () => {
+const pages = () => {
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen relative">
-      {/* Left Sidebar */}
-      <div className="hidden lg:block lg:w-1/4 xl:w-1/5 fixed left-0">
+    <div className="flex w-full">
+      <div className="w-1/4">
         <Leftside />
       </div>
-
-      {/* Main Feed */}
-      <div className="w-full lg:w-1/2 xl:w-3/5 lg:ml-[25%] xl:ml-[20%] lg:mr-[25%] xl:mr-[20%]">
+      <div className="w-full">
         <Feed />
       </div>
-
-      {/* Right Sidebar */}
-      <div className="hidden lg:block lg:w-1/4 xl:w-1/5 fixed right-0">
+      <div className="w-1/4">
         <Rightside />
       </div>
     </div>
   );
 };
 
-export default Pages;
+export default pages;
